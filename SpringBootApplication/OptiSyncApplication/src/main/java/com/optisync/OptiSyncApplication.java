a91bb89254c2da8b11cb3ffd1fee164d9def64c8
Abhi-1398 <136895740+Abhi-1398@users.noreply.github.com>
2023-08-29 21:32:52 +0530

    branch abhi

---
+++ SpringBootApplication/OptiSyncApplication/src/main/java/com/optisync/OptiSyncApplication.java
@@ -0,0 +1,13 @@
+package com.optisync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiSyncApplication.class, args);
+	}
+
+}
---
+++ SpringBootApplication/OptiSyncApplication/src/test/java/com/optisync/OptiSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
