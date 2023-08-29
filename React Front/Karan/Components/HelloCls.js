9aa61afb3d4d4412cdbb0a964a645aab3926e441
Abhi-1398 <136895740+Abhi-1398@users.noreply.github.com>
2023-08-29 17:20:00 +0530

    updated Folder Struture

--- React Front/Karan/Components/HelloCls.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-class HelloCls extends React.Component{
-
-    render(){
-        return <h1> Hello from  class component</h1>
-    }
-}
-
-export default HelloCls;
--- SpringBootRestAPI/Karan/repositories/VendorMapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.VendorMap;
-
-public interface VendorMapRepository extends JpaRepository<VendorMap, Integer> {
-
-}
