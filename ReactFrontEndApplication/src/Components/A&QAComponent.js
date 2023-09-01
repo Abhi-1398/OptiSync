736bfe43e63b779d0a27d61de31d28e317fa25c7
Abhi-1398 <136895740+Abhi-1398@users.noreply.github.com>
2023-09-01 14:44:23 +0530

    react A&QA component added

--- ReactFrontEndApplication/src/Components/A&QAComponent.js
+++ ReactFrontEndApplication/src/Components/A&QAComponent.js
@@ -1,34 +1,65 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import QualityAssemblyComp from "./QualityAssemblyComponent";
 let AssemblyQA=()=>
 {
     return(
-        <nav className="navbar navbar-expand-md navbar-light bg-light sidebar">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#leftNavbar">
-            <span className="navbar-toggler-icon"></span>
-            </button>
+        <div>
+      <div class="col-md-4 ">
+      <nav className="navbar navbar-expand-md navbar-light bg-light sidebar">
+        
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#leftNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+  
+        
+        <div className="collapse navbar-collapse" id="leftNavbar">
+          <ul className="navbar-nav flex-column">
+          
+       
+    <li className="nav-item">
+      <Link to="/receivedp" className="nav-link px-3">Received Part from production</Link>
+    </li>
 
-            <div className="collapse navbar-collapse" id="leftNavbar">
-            <ul className="navbar-nav flex-column">
-                
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">
-                    Check Part List
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">
-                    Daily Task
-                    </a>
-                    </li>
-                    
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">
-                    Forward Raw Material 
-                    </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <li className="nav-item">
+      <Link to="/plantaskAQA" className="nav-link px-3">Plan Daily Task</Link>
+    </li>
+
+    <li className="nav-item">
+      <Link to="/forwordToD" className="nav-link px-3">Forward Parts to Dispatch</Link>
+    </li>
+
+   
+
+
+   
+   
+         </ul>
+        </div>
+       <div>
+            <Routes>
+             {/* <Route path='receivedp' element={<AssemblyQA/>}/>
+             <Route path='plantaskAQA' element={<AandQAPlanTask/>}/>
+     */}
+            <Route path='receivedp' element={<AandQAReceivedQty/>}/>
+    
+             <Route path='plantaskAQA' element={<AandQAPlanTask/>}/>
+            
+             </Routes>
+            
+            
+            
+            </div> 
+            </nav>
+</div>
+  </div>
+        
+
     );
 };
 
